dc44d34ac369c843c901271f1e33cde0749d4936
coach-oox <coachmsoox@gmail.com>
2021-02-27 23:27:49 +0900

    Upload

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,16 +2,16 @@ import gulp from "gulp";
 import sass from "gulp-sass";
 sass.compiler = require("node-sass");
 
-const convertScss = () => {
+function convertScss() {
     return gulp
         .src("./src/scss/*.scss")
         .pipe(sass().on("error", sass.logError))
         .pipe(gulp.dest("./src/css"));
-};
+}
 
-const watchScss = () => {
-    gulp.watch("./src/scss/*.scss", gulp.series("scss"));
-};
+function watchScss() {
+    gulp.watch("./src/scss/*.scss", gulp.series(convertScss));
+}
 
 gulp.task("scss", convertScss);
 gulp.task("watch", watchScss);
